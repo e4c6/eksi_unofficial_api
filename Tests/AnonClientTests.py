10d0049ff81c142937cc9e3d96dc89d419a4a614
e4c6 <e4c6@dataso.me>
2021-11-28 11:55:11 +0300

    create tests, add requirements.txt

---
+++ Tests/AnonClientTests.py
@@ -0,0 +1,31 @@
+import pytest
+
+from EksiSozluk import EksiApi
+
+api = None
+
+@pytest.mark.dependency()
+def test_anon_client():
+    global api
+    api = EksiApi()
+    assert api.token is not None
+
+
+@pytest.mark.dependency(depends=['test_anon_client'])
+def test_anon_client_get_entry():
+    global api
+    entry = api.get_entry(1)
+    assert (entry.id == 1)
+
+
+@pytest.mark.dependency(depends=['test_anon_client'])
+def test_anon_client_get_topic():
+    global api
+    topic = api.get_topic(7006370)
+    assert topic.success
+
+
+@pytest.mark.dependency(depends=['test_anon_client'])
+def test_anon_client_get_user():
+    user = api.get_user("nimzo")
+    assert user.user_info.user_identifier.nick == "nimzo"
