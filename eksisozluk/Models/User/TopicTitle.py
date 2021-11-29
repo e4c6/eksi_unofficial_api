98f4d3b4dd1db7b085525ad5804092d07e3485a5
e4c6 <e4c6@dataso.me>
2021-11-29 17:10:28 +0300

    fix topic title, entries response, prep 1.0.6

--- eksisozluk/Models/User/TopicTitle.py
+++ eksisozluk/Models/User/TopicTitle.py
@@ -1,23 +1,21 @@
-from dataclasses import dataclass
-from typing import Any
-
+from eksisozluk.Models.__init__ import *
 from eksisozluk.Models import from_str, from_none
 
 
 @dataclass
 class TopicTitle:
-    title: str
-    kind: None
+    title: Optional[str] = None
+    kind: Optional[str] = None
 
     @staticmethod
     def from_dict(obj: Any) -> 'TopicTitle':
         assert isinstance(obj, dict)
-        title = from_str(obj.get("Title"))
-        kind = from_none(obj.get("Kind"))
+        title = from_union([from_str, from_none], obj.get("Title"))
+        kind = from_union([from_str, from_none], obj.get("Kind"))
         return TopicTitle(title, kind)
 
     def to_dict(self) -> dict:
         result: dict = {}
-        result["Title"] = from_str(self.title)
-        result["Kind"] = from_none(self.kind)
+        result["Title"] = from_union([from_str, from_none], self.title)
+        result["Kind"] = from_union([from_str, from_none], self.kind)
         return result
